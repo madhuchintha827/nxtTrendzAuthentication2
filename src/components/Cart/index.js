e88ad5d71bba9beb5fa8665ce2212640f6931c86
madhuchintha827 <chinthamadhu827@gmail.com>
2022-12-22 07:06:58 +0000

    solution

---
+++ src/components/Cart/index.js
@@ -0,0 +1,14 @@
+// Write your JS code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="cart-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="cart-img"
+    />
+  </div>
+)
+
+export default NotFound
---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const NotFound = () => (
+  <div className="product-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+      alt="products"
+      className="product-img"
+    />
+  </div>
+)
+
+export default NotFound
